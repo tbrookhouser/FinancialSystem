66e0169f304b765775cd7ea0d3c37bf40a4f8e92
mwitzenburg <maggie.witzenburg@gmail.com>
2015-02-10 21:20:02 -0600

    added xstream

--- src/PersonLog.java
+++ src/PersonLog.java
@@ -7,13 +7,16 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Scanner;
 
+import com.thoughtworks.xstream.XStream;
+
 
 
 public class PersonLog {
 		
 		//creates a new ArrayList to store people
 		ArrayList<Person> personList = new ArrayList<Person>();
-		WritePersonXML writePersonXml = new WritePersonXML();
+		//WritePersonXML writePersonXml = new WritePersonXML();
+		XStream xstream = new XStream();
 		
 		/**
 		 * returns the person array List
@@ -73,7 +76,7 @@ public class PersonLog {
 				System.out.println(line);
 				Person person = parsePerson(line);
 				personList.add(person);
-				writePersonXml.writeToPersonXml(person);
+				String xml = xstream.toXML(person);
 			}
 		}
 		
